08771f564c92bedbd12d1b744f09f085b49f4569
Habeeb Alabi <habeeb.olamighty22@gmail.com>
2023-03-13 22:11:34 +0000

    initial

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,5 +1,2 @@
 #!/usr/bin/node
-//scripts that prints two arguments passed to it
-const process = require('process');
-let args = process.argv;
-console.log(args[2] + " is " + args[3]);
+console.log(process.argv[2] + ' is ' + process.argv[3]);
