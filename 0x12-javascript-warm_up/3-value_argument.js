b4d445177937bbd245f11fffbb1025dc77e42bca
Habeeb Alabi <habeeb.olamighty22@gmail.com>
2023-03-13 21:48:16 +0000

    initial

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
-//script that prints the first argument passed to it
-
-const process = require('process');
-let args = process.argv;
-if(args[2]){
-    console.log(args[2]);
-} else{
-    console.log('No argument');
+if (process.argv[2] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(process.argv[2]);
 }
