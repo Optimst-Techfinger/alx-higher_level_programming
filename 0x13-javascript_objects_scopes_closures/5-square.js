32d62ca139c1d18237d6733c0dbdc2c3aaead925
Habeeb Alabi <habeeb.olamighty22@gmail.com>
2023-03-14 22:47:07 +0000

    initial

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
+      return this;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+   print () {
+    let i = 0;
+    while (i < this.height) {
+        console.log('X'.repeat(this.width));
+        i++;
+    }
+   }
+    double () {
+        this.width = this.width * 2;
+        this.height = this.height * 2;
+    }
+    rotate () {
+        let holder = this.width;
+        this.width = this.height;
+        this.height = holder;
+    }
+}
+
+class Square extends Rectangle {
+    constructor (size) {
+        super(size, size);
+        this.size = size;
+    }
+}
+module.exports = Rectangle;
+module.exports = Square;
---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const newSquare = require ('./5-square.js');
+class Square extends newSquare {
+    charPrint(c) {
+        for (let i = 0; i < this.height; i++) {
+            if (c === undefined) {
+                console.log('X'.repeat(this.width));
+            } else {
+                console.log('C'.repeat(this.width));
+            }
+        }
+    };
+}
+module.exports = Square;
